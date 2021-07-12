6203057dc94d78fdd4c3fb083a6b1440af3f6469
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-07-12 21:18:10 +0530

    Demonstrate Async nature of javascript

---
+++ AjaxServerCalls.js
@@ -0,0 +1,11 @@
+function showTime(){
+    const date = new Date();
+    return date.getHours()+"Hrs " + date.getMinutes()+"Mins " + date.getSeconds()+ "Secs ";
+}
+function showSessionExpire(){
+    console.log("Activity-B: Your Session expired at " + showTime());
+}
+
+console.log("Activity-A: Trigerring activity-B at " + showTime());
+setTimeout(showSessionExpire,5000);
+console.log("Activity-A: Trigerring activity-B at " + showTime()+" will execute after 5 seconds");
